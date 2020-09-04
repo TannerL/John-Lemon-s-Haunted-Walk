9b4c2c97eb62358f5da57b13b880a1fc6e5b5f6e
TannerL <tannerlaw@weber.edu>
2020-09-03 19:51:59 -0600

    added PlayerMovement script(uncompleted.

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    Vector3 m_Movement;
+    Animator m_Animator;
+    public float turnSpeed = 20f;
+    Quaternion m_Rotation = Quaternion.identity;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        m_Animator = GetComponent<m_Animator>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        m_Movement.Set(horizontal, 0f, vertical);
+        m_Movement.Normalize();
+
+        bool hasHorizontalInput = !Mathf.Approximately(horizontal, 0f);
+        bool hasVerticalInput = !Mathf.Approximately(vertical, 0f);
+
+        bool isWalking = hasHorizontalInput || hasVerticalInput;
+        m_Animator.SetBool("IsWalking", isWalking);
+
+        Vector3 desiredForward = Vector3.RotateTowards(transform.forward, m_Movement, turnSpeed * Time.deltaTime, 0f);
+        m_Rotation = Quaternion.LookRotation(desiredForward);
+
+
+    }
+}
