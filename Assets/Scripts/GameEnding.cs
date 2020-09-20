3770749128ccae670439d13953c249358a29bcec
TannerL <tannerlaw@weber.edu>
2020-09-19 20:19:21 -0600

    finished part 7

--- Assets/Scripts/GameEnding.cs
+++ Assets/Scripts/GameEnding.cs
@@ -1,15 +1,18 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameEnding : MonoBehaviour
 {
     public float fadeDuration = 1f;
     public GameObject player;
     public float displayImageDuration = 1f;
-    bool m_IsPlayerAtExit;
+    bool m_IsPlayerAtExit = false;
     float m_timer;
     public CanvasGroup exitBackgroundImageCanvasGroup;
+    public CanvasGroup caughtBackgroundImageCanvasGroup;
+    bool m_IsPlayerCaught;
 
     void OnTriggerEnter (Collider other)
     {
@@ -19,21 +22,36 @@ public class GameEnding : MonoBehaviour
         }
     }
 
+    public void CaughtPlayer()
+    {
+        m_IsPlayerCaught = true;
+    }
+
     void Update ()
     {
         if (m_IsPlayerAtExit)
         {
-            EndLevel();
+            EndLevel(exitBackgroundImageCanvasGroup, false);
+        }
+        else if (m_IsPlayerCaught)
+        {
+            EndLevel(caughtBackgroundImageCanvasGroup, true);
         }
     }
 
-    void EndLevel()
+    void EndLevel(CanvasGroup imageCanvasGroup, bool doRestart)
     {
         m_timer += Time.deltaTime;
-        exitBackgroundImageCanvasGroup.alpha = m_timer / fadeDuration;
+        imageCanvasGroup.alpha = m_timer / fadeDuration;
         if(m_timer > fadeDuration + displayImageDuration)
         {
-            Application.Quit();
+            if (doRestart) {
+                SceneManager.LoadScene(0);
+            }
+            else
+            {
+                Application.Quit();
+            }
         }
     }
 }
